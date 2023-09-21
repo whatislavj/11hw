924691ea557ef2370d2dff0ebb06ec7887467b06
whatislavj <vlad.jechel@gmail.com>
2023-09-21 19:46:49 +0300

    Done

---
+++ index.js
@@ -0,0 +1,46 @@
+// task 2 
+var str = 'String';
+var numb = 2;
+var bool = true;
+var undef = undefined;
+var noval = null;
+var bignum = BigInt("66666666666666666666666666666666666");
+var obj = {name: 'Vlad'};
+var arr = ['Skoda', 'Toyota', 'BMW', 'Volkswagen', 'Porsche']; // oricum e object
+
+console.log(typeof str);
+console.log(typeof numb);
+console.log(typeof bool);
+console.log(typeof undef);
+console.log(typeof noval);
+console.log(typeof bignum);
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(' ');
+
+// task 3
+var num1 = 22;
+var num2 = 9;
+var num3 = 2006;
+console.log(num1 + num2 + num3);
+console.log(num1 === num2);
+console.log(num2 < num3);
+console.log(num2 > num3);
+console.log(num2 >= num3);
+console.log(num2 <= num3);
+console.log(' ');
+
+// task 4
+console.log('Hello' + ' ' + 'World');
+console.log(' ');
+
+// task 5
+var task5 = {
+    nume: 'nume',
+    prenume: 'prenume',
+    hobby: 'hobby',
+    varsta: 'varsta',
+    profesie: 'profesie'
+};
+
+console.log(task5)
